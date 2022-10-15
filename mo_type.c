8e8c69b5dc3a94beea5e0afba9c1b870ddb3b9f7
“ffeller” <flaris.feller@gmail.com>
2022-10-15 15:30:06 -0300

    Geracao de um share object apenas

--- mo_type.c
+++ mo_type.c
@@ -5,10 +5,62 @@
 
 #include "mastermobilitydb.h"
 
-PG_MODULE_MAGIC;
-
 PG_FUNCTION_INFO_V1(mo_type_create);
- 
+
+int run_sql_cmd(
+    char * op,
+    char * table,
+    char * sql,
+    Oid * types,
+    Datum * values
+) {
+{
+    SPIPlanPtr stmt; 
+    bool isnull;
+    int new_id, ret, proc;
+    
+    char * op = "insert";
+    char * table = "master.mo_type";
+
+    char * sql = 
+        "insert into master.mo_type(mo_type_id, description) \
+        values(nextval('master.seq_mo_type'), $1) \
+        returning mo_type_id";
+
+    SPI_connect();
+
+    stmt = SPI_prepare(sql, argcount, types);
+    if (!stmt) {
+        elog(ERROR, ERR_MMDB_001, op, table);
+    }
+
+    for (int i = 0; i < argcount; i++) {
+        values[i] = PG_GETARG_DATUM(i);
+    }
+
+    ret = SPI_execp(stmt, values, " ", 1);
+    if (ret < 0) {
+        elog(ERROR, ERR_MMDB_002, op, table);
+    }
+    proc = SPI_processed;
+
+    if (proc > 0) {
+        new_mo_type_id = DatumGetInt32(SPI_getbinval(SPI_tuptable->vals[0],
+                                    SPI_tuptable->tupdesc,
+                                    1,
+                                    &isnull));
+    } else {
+        elog(ERROR, ERR_MMDB_003, op, table);
+        new_mo_type_id = 0;
+    }
+
+    SPI_freeplan(stmt);
+    SPI_finish();
+    free(values);
+
+    PG_RETURN_INT32(new_mo_type_id);
+}
+
 Datum 
 mo_type_create(PG_FUNCTION_ARGS)
 {
