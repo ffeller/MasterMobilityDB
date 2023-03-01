1f5342f4b96e46f781c5f020b1e9f3f2a8629748
flaris.feller <flaris.feller@digitro.com>
2023-03-01 15:00:04 -0300

    monta tabelas temporarias

--- dbutil.h
+++ dbutil.h
@@ -3,6 +3,7 @@
 
 #define SCHEMA_NAME "master"
 #define SQL_LENGTH 400
+#define OBJ_LENGTH 30
 #define BATCH_SIZE 10000
 
 #define ERR_MMDB_001 "Failure preparing %s on table %s.%s"
@@ -12,6 +13,16 @@
 #define ERR_MMDB_005 "Failure saving %s on table %s.%s"
 #define ERR_MMDB_006 "Failure disconnecting from SPI manager on %s on table %s.%s"
 #define ERR_MMDB_007 "Failure getting results from %s on table %s.%s"
+#define ERR_MMDB_008 "SPI_connect() must be called first"
+
+typedef struct {
+  char schema[OBJ_LENGTH];
+  char table[OBJ_LENGTH];
+  char attribute[OBJ_LENGTH];
+  char datatype[OBJ_LENGTH];
+  bool required;
+  bool pkey;
+} typ_table_s;
 
 char *str_lower(char *);
 char *operation(char *);
@@ -27,3 +38,4 @@ HeapTuple run_sql_query_tuple_args(PG_FUNCTION_ARGS, char *, char *);
 Datum * run_sql_cmd_new(char *, char *, Oid *, int, Datum *, char *, uint64 *);
 Datum * run_sql_cmd_args_new(PG_FUNCTION_ARGS, char *, char *, uint64 *);
 ArrayType * make_pg_array(Datum *, uint64);
+typ_table_s * get_table_structure(char *, char *);
